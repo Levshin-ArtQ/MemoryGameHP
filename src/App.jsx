ae48efc1d6aa3b24cfd757c8c427a7502bc9ed19
Levshin-ArtQ <114755543+Levshin-ArtQ@users.noreply.github.com>
2025-01-22 15:07:48 +0500

    log

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import GameBoard from './Game/GameBoard';
+import { useEffect } from 'react';
 import './App.css'
 
 function App() {
+  useEffect(() => {
+    console.log('App component mounted')
+  }, [])
+  
   return (
     <div className="App">
       <GameBoard width={4} height={3} />
